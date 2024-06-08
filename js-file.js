d48fbf68415472e2b6ba3b823bd21c93bd68fbec
tyzrichard <tyzrichard28@gmail.com>
2024-06-08 12:43:22 +0800

    First Commit Creates index.html, style.css and js-file.js files, created calculator UI, coded operate and individual operator functions

---
+++ js-file.js
@@ -0,0 +1,31 @@
+function add(first, second) {
+    return first + second;
+}
+
+function subtract(first, second) {
+    return first - second
+}
+
+function multiply(first, second) {
+    return first * second;
+}
+
+function divide(first, second) {
+    return first / second;
+}
+
+let num1 = null,
+    operator = null, 
+    num2 = null;
+
+function operate(num1, operator, num2) {
+    if (operator == "+") {
+        return add(num1, num2);
+    } else if (operator == "-") {
+        return subtract(num1, num2)
+    } else if (operator == "*") {
+        return multiply(num1, num2) 
+    } else {
+        return dividie(num1, num2)
+    }
+}
